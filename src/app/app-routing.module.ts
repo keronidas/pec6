0963b65c4c375c8d7e019446fb72d15900cea6f7
keronidas <diego.julian@kepar.es>
2024-06-26 23:18:48 +0200

    primer guardado

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: 'home',
+  loadChildren: () => import('./home/home.module').then(m => m.HomeModule)
+},
+{
+  path: 'list',
+  loadChildren: () => import('./list/list.module').then(m => m.ListModule)
+},
+{
+  path: 'statistic',
+  loadChildren: () => import('./statistic/statistic.module').then(m => m.StatisticModule)
+},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ListComponent } from './list/list.component';
+import { StatisticComponent } from './statistic/statistic.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ListComponent,
+    StatisticComponent
   ],
   imports: [
     BrowserModule,
